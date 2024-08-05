5afea095351f41fbd55052b4bce28af8d11b40ea
ricardina-ngovene <ngovenericardina27@gmail.com>
2024-08-05 13:02:45 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Login from "./pages/Login";
+import Footer from "./pages/footer";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>Ola mundo</div>
+      <Login/>
+      <Footer/>
     </>
   );
 }

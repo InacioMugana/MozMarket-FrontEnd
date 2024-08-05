296c14a528aa95d2147edf5aece7fb436cabf8b0
AtaliaPatissone <ataliadacostapatissone@gmail.com>
2024-08-05 14:27:22 +0200

    uniformizacao da lingungem

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,29 +2,29 @@
 export const Table = () => {
 
   const lojas = [
-    { nome: 'MozTech', produtos: 120, categorias: 'Eletrônicos, Móveis' },
-    { nome: 'Loja das Damas', produtos: 75, categorias: 'Roupas, Acessórios' },
-    { nome: 'Moz Academic', produtos: 200, categorias: 'Livros, Papelaria' },
-    { nome: 'Shoprit', produtos: 50, categorias: 'Alimentos, Bebidas' },
+    { name: 'MozTech', products: 120, categories: 'Electronics, Furniture' },
+    { name: 'Ladies', Store, products: 75, categories: 'Clothing, Accessories' },
+    { name: 'Moz Academic', products: 200, categories: 'Books, Stationery' },
+    { Name: 'Shoprit', products: 50, categories: 'Food, Beverages' },
   ];
 
   return (
     <div className="bg-white shadow rounded-lg p-6 mt-6">
-      <h2 className="text-2xl font-bold mb-4">Lojas</h2>
+      <h2 className="text-2xl font-bold mb-4">Stores</h2>
       <table className="w-full text-left table-auto">
         <thead>
           <tr className="bg-[#D8F0FD]">
-            <th className="px-4 py-2">Nome da Loja</th>
-            <th className="px-4 py-2">Número de Produtos</th>
-            <th className="px-4 py-2">Categorias</th>
+            <th className="px-4 py-2">Store Name</th>
+            <th className="px-4 py-2">Number of Products</th>
+            <th className="px-4 py-2">Categories</th>
           </tr>
         </thead>
         <tbody>
           {lojas.map((loja, index) => (
             <tr key={index} className="border-t border-gray-300">
-              <td className="px-4 py-2">{loja.nome}</td>
-              <td className="px-4 py-2">{loja.produtos}</td>
-              <td className="px-4 py-2">{loja.categorias}</td>
+              <td className="px-4 py-2">{loja.name}</td>
+              <td className="px-4 py-2">{loja.products}</td>
+              <td className="px-4 py-2">{loja.categories}</td>
             </tr>
           ))}
         </tbody>
--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -1,27 +1,27 @@
 export const Summary = () => {
   return (
     <div  className="flex  flex-col bg-white shadow rounded-md">
-      <h1 className="font-semibold text-xl pl-9">Resumo</h1>
+      <h1 className="font-semibold text-xl pl-9">Resume</h1>
       <div className="flex justify-between items-center p-8 ">
         <div className="flex-1 bg-[#F3E8FF] shadow rounded-lg p-6 m-2">
-          <h2 className="text-xl font-bold mb-2">Produtos</h2>
+          <h2 className="text-xl font-bold mb-2">Products</h2>
           <p className="text-3xl font-semibold">150</p>
-          <p className="text-sm text-gray-500">Total de produtos disponíveis</p>
+          <p className="text-sm text-gray-500">Total products available</p>
         </div>
         <div className="flex-1 bg-[#FFE2E5] shadow rounded-lg p-6 m-2">
-          <h2 className="text-xl font-bold mb-2">Vendas Totais</h2>
+          <h2 className="text-xl font-bold mb-2">Total Sales</h2>
           <p className="text-3xl font-semibold">$ 50,000</p>
-          <p className="text-sm text-gray-500">Receita gerada</p>
+          <p className="text-sm text-gray-500">Revenue generated</p>
         </div>
         <div className="flex-1 bg-[#FFF4DE] shadow rounded-lg p-6 m-2">
-          <h2 className="text-xl font-bold mb-2">Categorias</h2>
+          <h2 className="text-xl font-bold mb-2">Categories</h2>
           <p className="text-3xl font-semibold">8</p>
-          <p className="text-sm text-gray-500">Categorias de produtos</p>
+          <p className="text-sm text-gray-500">Product Categories</p>
         </div>
         <div className="flex-1 bg-[#DCFCE7] shadow rounded-lg p-6 m-2">
-          <h2 className="text-xl font-bold mb-2">Clientes</h2>
+          <h2 className="text-xl font-bold mb-2">Costumer</h2>
           <p className="text-3xl font-semibold">1,200</p>
-          <p className="text-sm text-gray-500">Clientes ativos</p>
+          <p className="text-sm text-gray-500">Active customers</p>
         </div>
       </div>
     </div>

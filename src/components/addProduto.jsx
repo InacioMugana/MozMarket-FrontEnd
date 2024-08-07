46dfddc1b8b0b33bcc72b7ee4e0992cbbe0aa076
Francisca Uamba <franciscauamba@gmail.com>
2024-08-07 00:21:59 -0700

    update:add-produt-route

--- src/components/addProduto.jsx
+++ src/components/addProduto.jsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom"
 
 
 export const AddProduto = () => {
     return (
         <div className="p-6">
-        <h1 className="text-2xl font-bold mb-4">Add Product</h1>
-       
+          
+        <h1 className="text-2xl font-bold text-center mb-4">Add Product</h1>
+        <Link to="/" className="inline-block  absolute top-2 left-2 ">
+        <button className=" bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+        Home page
+        </button>
+      </Link>
         <div className="flex items-center mb-6">
           <div className="ml-auto">
             <button className="border border-red-500 text-red-500 px-4 py-2 rounded mr-2">Discard Changes</button>
